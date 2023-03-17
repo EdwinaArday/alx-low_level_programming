16a012437da5e9140264414efabc0ca5824f6d6d
EdwinaArday <ardayedwina@gmail.com>
2023-03-17 00:47:40 -0700

    My commit for 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(void)
+/**
+* main - main entry point
+* return : should always return to 0
+*/
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+

23ed2d9b00de8bbe2982cb2bec37548e1d9501c7
EdwinaArday <ardayedwina@gmail.com>
2023-03-20 06:31:31 -0700

    My commit for -Wall -pedantic -Werror -Wextra -std=gnu89 0-positive_or_negative.c -o 0-positive_or_negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,8 +1,12 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* main -main entry point
+/*
+ * main -main entry point
  * Return: should always return 0
+*/
+/*
+ * betty style doc for function main
 */
 int main(void)
 {
@@ -10,7 +14,7 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	 if (n > 0)
+	if (n > 0)
 	{
 		printf("%d is positive\n", n);
 	}

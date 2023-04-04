703dba69e549ce0670ce2fea4c638dd895e7201c
EdwinaArday <ardayedwina@gmail.com>
2023-04-04 02:08:23 -0700

    My commit for 6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -3,17 +3,18 @@
 /**
  * _abs - computes the absolute value of an integer.
  * @c: the number to be computed.
- * Return: Absolute value of nnumber or zero
+ * Return: Absolute value of number or zero
  */
 
 int _abs(int c)
 {
+
 	if (c > 0)
 	{
-		int abs_val;
+	int abs_val;
 
-		abs_val = c * -1;
-		return (abs_val);
+	abs_val = c * -1;
+	return (abs_val);
 	}
 	return (c);
 }

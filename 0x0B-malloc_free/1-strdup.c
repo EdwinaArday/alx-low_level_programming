1355a4db213e3262a4be70af190eefb6676d4172
EdwinaArday <ardayedwina@gmail.com>
2023-04-13 05:01:38 -0700

    My commit for 1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _strdup - make another copy to the new memory space loaction
+ * @str: char
+ * Return: 0
+ */
+char *_strdup(char *str)
+{
+	char *y;
+	int i, r = 0;
+
+	if (str == NULL)
+		return (NULL);
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+
+	y = malloc(sizeof(char) * (i + 1));
+
+	if (y == NULL)
+		return (NULL);
+	for (r = 0; str[r]; r++)
+		y[r] = str[r];
+	return (y);
+}

a59cbde4b8d2b01f3c7882524b551691e6eb8732
EdwinaArday <ardayedwina@gmail.com>
2023-04-21 03:25:29 -0700

    My commit for 2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -9,13 +9,15 @@
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int a;
-	if (array = NULL || siez <= 0 || cmp == NULL)
+	int i;
+
+	if (array == NULL || size <= 0 || cmp == NULL)
 		return (-1);
-	for (a = 0; a < size; a++)
+
+	for (i = 0; i < size; i++)
 	{
-		if ((cmp(array[a]))
-				return (a);
+		if (cmp(array[i]))
+			return (i);
 	}
 	return (-1);
 }
